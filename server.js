49707d89262ade56392e2cbfb6a3f4f494aa57dd
Joe Azure <jazure@gmail.com>
2022-12-19 16:24:49 -0700

    Some more traits

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+ // origin: "http://localhost:8080"
 };
 
 app.use(cors(corsOptions));
